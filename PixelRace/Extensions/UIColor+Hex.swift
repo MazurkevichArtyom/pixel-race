3503efa87f7c4e9b21c00e8092ebdfafbe3b2118
Artem Mazurkevich <artyom.mazurkevich@gmail.com>
2022-02-12 17:10:07 +0300

    Add hex extension for UIColor

---
+++ PixelRace/Extensions/UIColor+Hex.swift
@@ -0,0 +1,13 @@
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    convenience init(hex: Int) {
+        let red = (hex >> 16) & 0xFF
+        let green = (hex >> 8) & 0xFF
+        let blue = hex & 0xFF
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1)
+    }
+}
